22aed391c19e3dc2e14bbdefd23cb5d26ef542cb
baash05 <david.rawk@gmail.com>
2014-04-05 23:24:38 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Read2mykids::Application.config.secret_token = '439115a53982ce31236e1824a1ba476c45bb0c895dbf3b240c2097d59ca4a7196c3d4e44892d426d52d73218ddd09cb0734731824478d06b36ef30882f7f90fe'
